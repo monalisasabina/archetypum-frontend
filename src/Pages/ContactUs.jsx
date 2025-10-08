afe63d0cd2393fe8fa20695e4028af02a662c92d
Monalisa Sabina <onyangomonalisa@hotmail.com>
2025-10-08 17:45:05 +0300

    contact details and overall font

--- src/Pages/ContactUs.jsx
+++ src/Pages/ContactUs.jsx
@@ -1,7 +1,17 @@
 import PhoneInput from "react-phone-input-2"
 import "react-phone-input-2/lib/style.css"
 import "./contactUs.css"
+
 import { useState } from "react"
+import { FaFacebook } from "react-icons/fa6";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa6";
+import { FaInstagramSquare } from "react-icons/fa";
+import { HiOutlineDevicePhoneMobile } from "react-icons/hi2";
+import { MdOutlineEmail } from "react-icons/md";
+import { GiPostOffice } from "react-icons/gi";
+import { MdOutlineLocationOn } from "react-icons/md";
+import { FaRegClock } from "react-icons/fa";
 
 function ContactUs(){
 
@@ -25,8 +35,67 @@ function ContactUs(){
 
            {/* Contact-Content */}
            <div className="contact-content">
-              
-            
+
+             {/* Company Contacts */}
+              <div className="contact-contacts">
+                <h3>Get in Touch</h3>
+                
+                <div className="contact-details">
+                  <HiOutlineDevicePhoneMobile/>
+                  <div className="info-text">
+                    <p> <strong>Mobile:</strong></p>
+                    <p>+254 700 000 000</p>
+                  </div>
+                </div>
+
+                <div className="contact-details">
+                  <MdOutlineEmail/>
+                  <div className="info-text">
+                    <p><strong>Email:</strong></p>
+                    <p>archetypum@example.com</p>
+                  </div>  
+                </div>
+
+                <div className="contact-details">
+                  <GiPostOffice/>
+                  <div className="info-text">
+                    <p><strong>Post Office:</strong></p>
+                    <p>123 Archetypum P.O. Box, Nairobi, Kenya</p>
+                  </div>
+                </div>
+
+                <div className="contact-details">
+                  <MdOutlineLocationOn/>
+                  <div className="info-text">
+                    <p><strong>Location:</strong></p>
+                    <p>123 Archetypum Street, Nairobi, Kenya</p>
+                    <a 
+                      href="https://maps.app.goo.gl/PNKmRW3ZBdFcnxjp8" 
+                      target="_blank" 
+                      rel="noopener noreferrer"
+                     >View on Map
+                    </a>
+                  </div>
+                </div>
+
+                 <div className="contact-details">
+                  <FaRegClock /> 
+                  <div className="info-text">
+                    <p><strong>Working Hours:</strong></p>
+                    <p>Mon - Fri: 9:00 AM - 5:00 PM</p>
+                  </div>
+                </div>
+
+                <div className="contact-details social-icons">
+                  <p><strong>Follow Us:</strong></p>
+                  <a href="https://www.facebook.com/archetypum" target="_blank" rel="noopener noreferrer"><FaFacebook /></a>
+                  <a href="https://twitter.com/archetypum" target="_blank" rel="noopener noreferrer"><FaSquareXTwitter /></a>
+                  <a href="https://www.linkedin.com/company/archetypum" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
+                  <a href="https://www.instagram.com/archetypum" target="_blank" rel="noopener noreferrer"><FaInstagramSquare /></a>
+                </div>
+
+              </div>  
+
              {/* Contact Form */}
               <div className="contact-form">
 
@@ -66,7 +135,6 @@ function ContactUs(){
                                 required: true,
                                 autoFocus: false
                             }}
-    
                        />
                     </div>
 
@@ -78,16 +146,11 @@ function ContactUs(){
 
                     {/* Send Message */}
                     <button>Send Message</button>
-
                 </form>
 
               </div>
 
-              {/* Company Contacts */}
-              <div className="contact-contacts">
-                <h3>Company Contacts come on this side</h3>
-
-              </div>
+             
 
            </div>
 
