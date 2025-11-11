68e413e23085d86905f9e381b467c80d1bb442ff
Monalisa Sabina <onyangomonalisa@hotmail.com>
2025-11-11 17:04:21 +0300

    removed recent projects

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -11,17 +11,6 @@ import { Link } from "react-router-dom";
 
 const Projects = () => {
 
-    // Sort projects by date
-    const sortedProjects = [
-        ...projects].sort((a, b) => new Date(b.dateAdded) - new Date(a.dateAdded));
-    
-    
-    // Project functions
-        // Recent projects
-        const recentProjects = sortedProjects.slice(0, 3); 
-
-        // Other projects
-        const otherProjects = sortedProjects.slice(3); // Get the remaining projects
 
     // Capitalize category names
     const capitalizeCategory = (category) => {
@@ -37,74 +26,13 @@ const Projects = () => {
                 <p>An overview of completed developments demonstrating our capability in delivering functional, innovative, and high-quality architectural solutions.</p>
             </div>
 
-            {/* RECENT PROJECTS */}
-            <div className="recent-projects">
 
-                <h2 className="section-title">Recent Projects</h2>
-             
-                <Swiper
-                    modules={[Navigation, Pagination, Autoplay]}
-                    navigation
-                    pagination={{ clickable: true }}
-                    spaceBetween={5}
-                    slidesPerView={2}
-                    autoplay={{
-                        delay: 4000,   //4 seconds between slides
-                        disableOnInteraction: false,
-                    }}
-                    breakpoints={{
-                        0: {slidesPerView:1},   //mobile
-                        768: {slidesPerView:2},   //large tablets
-                        1024: {slidesPerView:2}   //desktop
-                    }}
-                  >
-                  {recentProjects.map((project) => (
-                     <SwiperSlide key={project.id}>
-
-                         <Link to={`/projects/${project.id}`} className="all-projects-link">
-                           <div className="project-card">
-
-                              {/* Project Thumbnail */}
-                              <div className="project-thumbnail">
-                                <img
-                                    src={project.thumbnail}
-                                    alt={`${project.title} Thumbnail`}
-                                />
-                                
-                              </div>
-
-                               {/* Project Content */}
-                              <div className="project-card-content">
-
-                                <h2>{project.title}</h2>
-                                <p className="project-subtitle">{project.location}</p>
-
-                                {/* Read More Button */}
-                                {/* <Link to={`/projects/${project.id}`} className="project-link">
-                                    <button className="project-button">Uncover More</button>
-                                </Link> */}
-
-                               </div>
-                        
-                            </div>
-                        </Link> 
-                     </SwiperSlide>
-                   ))}
-                 </Swiper>
-          
-            </div>
-            
-            
-
-            {/* OTHER PROJECTS */}
-            {otherProjects.length > 0 && (
+            {/* PROJECTS GRID */}
+            {projects.length > 0 && (
                 <div>
-                    <h2 className="section-other-title">Other Projects</h2>
-
-                    <p>Discover more spaces we have transformed through design and innovation.</p>
-
+                
                     <div className="projects-grid">
-                        {otherProjects.map((project) => (
+                        {projects.map((project) => (
 
                             <Link to={`/projects/${project.id}`} key={project.id} className="project-other-card-link">
 
